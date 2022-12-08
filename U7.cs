a67f1ad46e8dcc0be9b92ca320c55bf1092b0103
Kwok He Chu <>
2022-12-08 21:08:56 +0100

    Day 7 done

--- U7.cs
+++ U7.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,16 +8,29 @@ namespace AOC2022
     {
         private readonly AocHttpClient _client = new AocHttpClient(7);
 
-
         public void Execute1()
         {
             string input = _client.RetrieveFile().GetAwaiter().GetResult();
 
-            input = "$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k";
+            //input = "$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k";
             string[] split = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
 
-            DirectoryModel root = null;
-            DirectoryModel current = null;
+            DirectoryModel root = new DirectoryModel("/", null);
+            List<DirectoryModel> directories = BuildTreeOfDirectories(split, root);
+
+            long result = directories
+                .Where(dir => dir.GetSize() <= 100000)
+                .Sum(dir => dir.GetSize());
+
+            Console.WriteLine(result);
+        }
+
+        private List<DirectoryModel> BuildTreeOfDirectories(string[] split, DirectoryModel root)
+        {
+            DirectoryModel current = root;
+            List<DirectoryModel> result = new List<DirectoryModel>();
+            result.Add(root);
+
             for (int i = 0; i < split.Length; i++)
             {
                 string line = split[i];
@@ -33,36 +45,39 @@ namespace AOC2022
                             switch (directoryName)
                             {
                                 case "/": // Root
-                                    root = new DirectoryModel(directoryName, 0, null, DirectoryModelType.Directory);
                                     current = root;
                                     break;
                                 case "..": // cd previous
-                                    current = current.Previous;
+                                    if (current.Parent == null)
+                                        throw new NullReferenceException();
+                                    current = current.Parent;
                                     break;
                                 default: // cd into
-                                    current = FindByName(current, directoryName);
+                                    current = FindDirectoryByName(current, directoryName);
                                     break;
                             }
                             break;
                         case "ls":
                             for (int j = i + 1; j < split.Length; j++)
                             {
-                                var lsCommand = split[j].Split(' ');
+                                string[] lsCommand = split[j].Split(' ');
                                 string arg1 = lsCommand[0];
                                 string arg2 = lsCommand[1];
-
-                                if (arg1 == "$") // Command
+                                
+                                if (arg1 == "$")
                                 {
                                     i = j - 1;
                                     break;
                                 }
                                 else if (arg1 == "dir") // Directory
                                 {
-                                    current.Next.Add(new DirectoryModel(arg2, 0, current, DirectoryModelType.Directory));
+                                    DirectoryModel directory = new DirectoryModel(arg2, current);
+                                    current.Directories.Add(arg2, directory);
+                                    result.Add(directory);
                                 }
                                 else // File
                                 {
-                                    current.Next.Add(new DirectoryModel(arg2, long.Parse(arg1), current, DirectoryModelType.File));
+                                    current.Files.Add(arg2, long.Parse(arg1));
                                 }
                             }
                             break;
@@ -71,11 +86,7 @@ namespace AOC2022
                     }
                 }
             }
-
-            ComputeTotalSizePerDirectoryUsingDepthFirstSearch(root);
-            long result = FindMaxSizePerDirectory(root, 100000);
-
-            Console.WriteLine(result);
+            return result;
         }
 
         public void Execute2()
@@ -83,144 +94,46 @@ namespace AOC2022
             string input = _client.RetrieveFile().GetAwaiter().GetResult();
             string[] split = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
 
-            Console.WriteLine();
+            DirectoryModel root = new DirectoryModel("/", null);
+            List<DirectoryModel> directories = BuildTreeOfDirectories(split, root);
+            long freeSpace = 70000000 - root.GetSize();
+            long result = directories.Where(dir => dir.GetSize() > 30000000 - freeSpace)
+                .OrderBy(dir => dir.GetSize())
+                .FirstOrDefault()
+                .GetSize();
+
+            Console.WriteLine(result);
         }
 
-        private DirectoryModel FindByName(DirectoryModel root, string name)
+        private DirectoryModel FindDirectoryByName(DirectoryModel current, string name)
         {
-            Queue<DirectoryModel> queue = new Queue<DirectoryModel>();
-            queue.Enqueue(root);
-
-            while (queue.Count > 0)
-            {
-                DirectoryModel head = queue.Dequeue();
-                foreach (DirectoryModel directory in head.Next)
-                {
-                    if (directory.Name == name)
-                        return directory;
-
-                    queue.Enqueue(directory);
-                }
-            }
-
+            if (current.Directories.TryGetValue(name, out var directory))
+                return directory;
             throw new KeyNotFoundException();
-        }
-
-
-        private void ComputeTotalSizePerDirectoryUsingDepthFirstSearch(DirectoryModel start)
-        {
-            var visited = new HashSet<DirectoryModel>();
-
-            Stack<DirectoryModel> stack = new Stack<DirectoryModel>();
-            stack.Push(start);
-
-            while (stack.Count > 0)
-            {
-                var current = stack.Pop();
-
-                if (visited.Contains(current))
-                {
-                    continue;
-                }
-
-
-                visited.Add(current);
-
-                foreach (DirectoryModel neighhbour in current.Next)
-                {
-                    if (!visited.Contains(neighhbour))
-                    {
-                        stack.Push(neighhbour);
-                    }
-                }
-
-                current.Size += current.Next.Sum(x => x.GetSize());
-            }
-        }
-
-        private long FindMaxSizePerDirectory(DirectoryModel start, long maxSize)
-        {
-            long sum = 0;
-            var visited = new HashSet<DirectoryModel>();
-
-            Queue<DirectoryModel> queue = new Queue<DirectoryModel>();
-            queue.Enqueue(start);
-
-            while (queue.Count > 0)
-            {
-                var current = queue.Dequeue();
-
-                if (visited.Contains(current))
-                {
-                    continue;
-                }
-
-
-                visited.Add(current);
-
-
-                if (current.Size <= maxSize)
-                {
-                    sum += current.Size;
-                }
-                else
-                {
-                    foreach (DirectoryModel neighhbour in current.Next)
-                    {
-                        if (!visited.Contains(neighhbour))
-                        {
-                            queue.Enqueue(neighhbour);
-                        }
-                    }
-                }
-            }
-            return sum;
         }
     }
 
     public class DirectoryModel
     {
         public string Name { get; set; }
-        public long Size { get; set; }
-        public List<DirectoryModel> Next { get; set; }
-        public DirectoryModel Previous { get; set; }
-        public DirectoryModelType Type { get; set; }
+        public DirectoryModel Parent { get; set; }
+        public Dictionary<string, DirectoryModel> Directories = new Dictionary<string, DirectoryModel>();
+        public Dictionary<string, long> Files = new Dictionary<string, long>();
 
-        public DirectoryModel(string name, long size, DirectoryModel previous, DirectoryModelType type)
+        public DirectoryModel(string name, DirectoryModel parent)
         {
             Name = name;
-            Size = size;
-            Next = new List<DirectoryModel>();
-            Previous = previous;
-            Type = type;
+            Parent = parent;
         }
 
         public long GetSize()
         {
-            if (Type == DirectoryModelType.Directory)
-            {
-                foreach (DirectoryModel item in Next)
-                    return Size + item.GetSize();
-            }
-
-            if (Type == DirectoryModelType.File)
-            {
-                return Size;
-            }
-
-            return 0;
+            return Directories.Values.Sum(dir => dir.GetSize()) + Files.Values.Sum(file => file);
         }
 
         public override string ToString()
         {
-            return $"{Name} {Size} {Type}";
+            return $"{Name} {GetSize()}";
         }
     }
-
-    public enum DirectoryModelType
-    {
-        Directory,
-        File
-    }
-
 }
---
+++ U8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC2022
+{
+    public class U8
+    {
+        private readonly AocHttpClient _client = new AocHttpClient(8);
+
+        public void Execute1()
+        {
+            string input = _client.RetrieveFile().GetAwaiter().GetResult();
+            var split = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine();
+        }
+
+        public void Execute2()
+        {
+            string input = _client.RetrieveFile().GetAwaiter().GetResult();
+            var split = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+            
+            Console.WriteLine();
+        }
+    }
+}
